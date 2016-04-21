3542a4f9a83e3c3be4ad3b737aabcc98ad6fe08e
Tom Kasem <mrtomyum@gmail.com>
2016-04-21 20:48:59 +0700

    Add app.Logout

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -4,7 +4,6 @@ import (
 	"github.com/revel/revel"
 	"github.com/mrtomyum/user/app/models"
 	//"github.com/mrtomyum/user/app/routes"
-	//"golang.org/x/crypto/bcrypt"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -12,15 +11,15 @@ type App struct {
 	*revel.Controller
 }
 
-//func (c App) connected() *models.User {
-//	if c.RenderArgs["user"] != nil {
-//		return c.RenderArgs["user"].(*models.User)
-//	}
-//	if username, ok := c.Session["user"]; ok {
-//		return c.getUser(username)
-//	}te(
-//	return nil
-//}
+func (c App) connected() *models.User {
+	if c.RenderArgs["user"] != nil {
+		return c.RenderArgs["user"].(*models.User)
+	}
+	if username, ok := c.Session["user"]; ok {
+		return c.getUser(username)
+	}
+	return nil
+}
 
 func (c App) getUser(username string) *models.User {
 	user := new(models.User)
@@ -31,28 +30,10 @@ func (c App) getUser(username string) *models.User {
 	return user
 }
 
-//func (c App) Login(username, password string, remember bool) revel.Result {
-//	user := c.getUser(username)
-//	if user != nil {
-//		err := bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
-//		if err != nil {
-//			c.Session["user"] = username
-//			if remember {
-//				c.Session.SetDefaultExpiration()
-//			} else {
-//				c.Session.SetNoExpiration()
-//			}
-//			c.Flash.Success("ยินดีต้อนรับ "+ username)
-//			return  c.Redirect(User.Index)
-//		}
-//	}
-//	return c.Redirect(App.Login)
-//}
-
 func (c App) Index() revel.Result {
-	//if c.connected() != nil {
-	//	return c.Redirect(routes.User.Index())
-	//}
+	if c.connected() != nil {
+		return c.Redirect(User.Index)
+	}
 	c.Flash.Error("กรุณา login ก่อน")
 	return c.Render()
 }
@@ -75,5 +56,12 @@ func (c App) Login(username, password string, remember bool) revel.Result {
 
 	c.Flash.Out["username"] = username
 	c.Flash.Error("Login failed")
+	return c.Redirect(App.Index)
+}
+
+func (c App) Logout() revel.Result {
+	for k := range c.Session {
+		delete(c.Session, k)
+	}
 	return c.Redirect(App.Index)
 }
